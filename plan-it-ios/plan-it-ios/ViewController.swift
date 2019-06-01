58b939293ea27cb9bcf34054ed6329ac6b782a1e
Kirill <kfilipov@wakeapp.ru>
2019-06-01 18:20:53 +0300

    Initial commit

---
+++ plan-it-ios/plan-it-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  plan-it-ios
+//
+//  Created by Kirill Philipov on 6/1/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
